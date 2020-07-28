1334ced5b07b884b56522bc7c74031d999582bf3
Alex Vieira <alex.vieira30@etec.sp.gov.br>
2020-07-28 16:56:37 -0300

    Logo footer

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -4,8 +4,8 @@ import { FooterBase } from './styles';
 function Footer() {
   return (
     <FooterBase>
-      <a href="https://www.alura.com.br/">
-        <img src="https://www.alura.com.br/assets/img/alura-logo-white.1570550707.svg" alt="Logo Alura" />
+      <a href="https://languages-flix.vercel.app/">
+        <img src="https://fontmeme.com/permalink/200728/f9f003e3801bb368983d25d628bd550e.png" alt="Logo Languages Flix" />
       </a>
       <p>
         Orgulhosamente criado durante a
