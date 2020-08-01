4b40fec1b665c1a7d78a72465b07aff7cb70ae26
Alex Vieira <alex.vieira30@etec.sp.gov.br>
2020-07-31 22:23:40 -0300

    404 - Animation

--- src/index.js
+++ src/index.js
@@ -15,16 +15,12 @@ const Pagina404 = () => (
           <div class="col-sm-10 col-sm-offset-1  text-center">
             <div class="four_zero_four_bg">
               <h1 class="text-center ">404</h1>
-
             </div>
-
             <div class="contant_box_404">
               <h3 class="h2">
                 Parece que você está perdido!
 		          </h3>
-
-              <p>a página que você procura não existe!</p>
-
+              <p>A página que você procura não existe!</p>
               <a href="/" class="link_404">Voltar</a>
             </div>
           </div>
